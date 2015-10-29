55667ca74b643a5e21c553a1eab5dac08bb24770
Daniel Plaisted <daplaist@microsoft.com>
2015-10-29 09:51:52 -0700

    Update paket.targets to handle package restore of packages with MSBuild props/targets

--- testpackagerestore.bat
+++ testpackagerestore.bat
@@ -1,5 +1,5 @@
 rmdir /S /Q %~dp0packages
-msbuild %~dp0src\PaketTestApp\PaketTestApp.csproj
+msbuild %~dp0src\PaketTestApp\PaketTestApp.csproj %*
 
 @rem If the assembly is signed then the targets file from the StrongNamer package was included correctly
 sn -T %~dp0src\PaketTestApp\bin\Debug\Octokit.dll
